11a6df552f1c5b92be2b360907284e371edce406
PvsKrishnaMohan <pvskrishnamohan479@gmail.com>
2026-07-08 07:54:57 +0530

    third largest num

--- SecondLargestNum.js
+++ SecondLargestNum.js
@@ -1,14 +1,20 @@
-const arr =[10,3,6,1,7,9,2,99]
+const arr = [11,3,6,10,7,9,2,99]
 
 var FirstLarNum = -Infinity
 var SecLarNum = -Infinity
+// var ThirdLarNum = -Infinity
 
 for(let i=0; i<arr.length; i++){
     if(arr[i] > FirstLarNum){
+        ThirdLarNum = SecLarNum
         SecLarNum = FirstLarNum
         FirstLarNum = arr[i]
-    }else if (arr[i] >SecLarNum) {
+    } else if (arr[i] > SecLarNum) {
         SecLarNum = arr[i]
     }
+    // else if(arr[i]>ThirdLarNum){
+    //     ThirdLarNum = arr[i]
+    // }
 }
+
 console.log(SecLarNum)
